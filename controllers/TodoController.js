f9a4267286d135d469ca44bcf2aa19979f150118
AanandRimal <aanandrimalo@gmail.com>
2023-06-14 10:32:20 +0545

    first commit

---
+++ controllers/TodoController.js
@@ -0,0 +1,94 @@
+const Todo = require('../models/TodoModel')
+const mongoose = require('mongoose')
+
+// get all workouts
+const getTodos = async (req, res) => {
+  const Todos = await Todo.find({}).sort({createdAt: -1})
+  res.status(200).json(Todos)
+}
+
+// get a single workout
+const getTodo = async (req, res) => {
+  const { id } = req.params
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({error: 'No such Todo'})
+  }
+
+  const todo= await Todo.findById(id)
+
+  if (!todo) {
+    return res.status(404).json({error: 'No such Todo'})
+  }
+
+  res.status(200).json(todo)
+}
+
+// create a new workout
+const createTodo = async (req, res) => {
+  const {title, description} = req.body
+
+  let emptyFields = []
+
+  if (!title) {
+    emptyFields.push('title')
+  }
+  if (!description) {
+    emptyFields.push('description')
+  }
+  if (emptyFields.length > 0) {
+    return res.status(400).json({ error: 'Please fill in all fields', emptyFields })
+  }
+
+  // add to the database
+  try {
+    const todo = await Todo.create({ title, description })
+    res.status(200).json(todo)
+  } catch (error) {
+    res.status(400).json({ error: error.message })
+  }
+}
+
+// delete a workout
+const deleteTodo = async (req, res) => {
+  const { id } = req.params
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).json({error: 'No such Todo'})
+  }
+
+  const todo = await Todo.findOneAndDelete({_id: id})
+
+  if(!Todo) {
+    return res.status(400).json({error: 'No such Todo'})
+  }
+
+  res.status(200).json(todo)
+}
+
+// update a workout
+const updateTodo = async (req, res) => {
+  const { id } = req.params
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).json({error: 'No such todo'})
+  }
+
+  const todo = await Todo.findOneAndUpdate({_id: id}, {
+    ...req.body
+  })
+
+  if (!Todo) {
+    return res.status(400).json({error: 'No such todo'})
+  }
+
+  res.status(200).json(todo)
+}
+
+module.exports = {
+  getTodos,
+  getTodo,
+  createTodo,
+  deleteTodo,
+  updateTodo
+}
---
+++ routes/Todo.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const {
+  getTodos, 
+  getTodo, 
+  createTodo, 
+  deleteTodo, 
+  updateTodo
+} = require('../controllers/TodoController')
+
+const router = express.Router()
+
+// GET all workouts
+router.get('/', getTodos)
+
+// GET a single workout
+router.get('/:id', getTodo)
+
+// POST a new workout
+router.post('/', createTodo)
+
+// DELETE a workout
+router.delete('/:id', deleteTodo)
+
+// UPDATE a workout
+router.patch('/:id', updateTodo)
+
+module.exports = router
